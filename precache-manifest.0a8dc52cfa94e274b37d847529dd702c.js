6bef23dce7761383a1cad6e58e9dee77b4e3e821
siyuannnnn-hsy <1587935941@qq.com>
2019-10-14 15:38:14 +0800

    a

---
+++ precache-manifest.0a8dc52cfa94e274b37d847529dd702c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad5798442596730affd1cff98ea9179",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e72a531b002431c6e2ed",
+    "url": "/static/css/main.2a75f1d7.chunk.css"
+  },
+  {
+    "revision": "ffcd5e3fc539dbddb250",
+    "url": "/static/js/2.33785d06.chunk.js"
+  },
+  {
+    "revision": "e72a531b002431c6e2ed",
+    "url": "/static/js/main.77404d8a.chunk.js"
+  },
+  {
+    "revision": "628ea25a63cbc5b89a27",
+    "url": "/static/js/runtime-main.9f147cbc.js"
+  }
+]);
